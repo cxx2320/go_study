d3e5203472bdb8ff5077f33025e6bc1807bd2f77
cxx <cxx2320@foxmail.com>
2020-07-27 17:53:25 +0800

    array_sum

--- array_sum/main.go
+++ array_sum/main.go
@@ -6,12 +6,12 @@ func main() {
 	array := [3]float64{7.0, 8.5, 9.1}
 	x := Sum(&array) // Note the explicit address-of operator
 	// to pass a pointer to the array
-	fmt.Printf("The sum of the array is: %f", x)
+	fmt.Println("The sum of the array is: %f", x)
 }
 
-func Sum(a *[3]float64) (sum float64) {
+func Sum(a *[3]float64) (num float64) {
 	for _, v := range a { // derefencing *a to get back to the array is not necessary!
-		sum += v
+		num += v
 	}
-	return
+	return num
 }
