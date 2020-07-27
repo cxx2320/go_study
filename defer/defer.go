72c50c272aa48303ab74a7e8cf41eb273562ca71
cxx <cxx2320@foxmail.com>
2020-07-27 10:33:18 +0800

    defer.go 格式化

--- defer/defer.go
+++ defer/defer.go
@@ -5,16 +5,16 @@ import "fmt"
 // defer调用顺序，先声明的后调用（栈，先进后出）。init函数也采用了这样的方法
 
 func main() {
-    fmt.Printf("main")
-    defer aa()
-    bb()
-    defer bb()
+	fmt.Println("main")
+	defer aa()
+	bb()
+	defer bb()
 }
 
-func aa(){
-    fmt.Printf("A")
+func aa() {
+	fmt.Println("A")
 }
 
-func bb(){
-    fmt.Printf("B")
+func bb() {
+	fmt.Println("B")
 }
