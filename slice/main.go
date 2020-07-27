d24e82d08f34a87f388950d95d516c65216a9183
cxx <cxx2320@foxmail.com>
2020-07-27 17:51:24 +0800

    slice

---
+++ slice/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	b := []string{"g", "o", "l", "a", "n", "g"}
+
+	// 带前不带后
+
+	// fmt.Println(b[1:4])
+
+	// fmt.Println(b[:2])
+
+	// fmt.Println(b[2:])
+
+	// 全部
+	fmt.Println(b[:])
+
+}
