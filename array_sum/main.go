a6c94f4ec5081157c87725b3c927a842f503f9a5
cxx <cxx2320@foxmail.com>
2020-07-30 18:19:22 +0800

    另一种函数返回值的方式

--- array_sum/main.go
+++ array_sum/main.go
@@ -4,14 +4,17 @@ import "fmt"
 
 func main() {
 	array := [3]float64{7.0, 8.5, 9.1}
-	x := Sum(&array) // Note the explicit address-of operator
+	x, cxx := Sum(&array) // Note the explicit address-of operator
 	// to pass a pointer to the array
 	fmt.Println("The sum of the array is: %f", x)
+	fmt.Println(cxx)
 }
 
-func Sum(a *[3]float64) (num float64) {
+// 如果返回值有变量，无需放在return后面就能返回
+func Sum(a *[3]float64) (num float64, cxx string) {
 	for _, v := range a { // derefencing *a to get back to the array is not necessary!
 		num += v
 	}
-	return num
+	cxx = "Dxx"
+	return
 }
